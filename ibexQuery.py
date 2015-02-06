54cb5cc888a254e40bdecc50c17d0fbe4396a1e2
mandrewcito <anbaalo@gmail.com>
2015-02-06 10:24:27 +0100

    actualizar solo cuando hay valores nuevos

--- ibexQuery.py
+++ ibexQuery.py
@@ -4,10 +4,25 @@ import model.getData.getIbex as getIbex
 import getDao as g
 import time
 import UTIL.dateUtil as dt
-import config
+import ConfigParser
+
+def setLastUpdate(fecha):
+  config = ConfigParser.ConfigParser()
+  config.read("config.ini")
+  cfgfile = open("config.ini",'w')
+  config.set('actualizacion','lastUpdate',fecha)
+  config.write(cfgfile)
+  cfgfile.close()
+
+def getLastUpdate():
+  config = ConfigParser.ConfigParser()
+  config.read("config.ini")
+  return config.get("actualizacion",'lastUpdate')
 
 def estaActualizado(fechaActualizacion):
-  return dt.esMasReciente(fechaActualizacion,config.lastUpdate)
+  lastUpdate=getLastUpdate()
+  print lastUpdate,fechaActualizacion
+  return dt.esMasReciente(fechaActualizacion,lastUpdate)
 
 def insertar(dao,date,(company,value)):
   dao.insert(date,company,value)
@@ -17,10 +32,12 @@ def main():
   if not estaActualizado(fechaActualizacion):
     print "los valores no han sido actualizados"
     return -1
+  else:
+    setLastUpdate(fechaActualizacion)
   #else actualizar fecha!
   dao=g.DaoUtil()
   for valor in lista_valores :
-    insertar(dao,date,valor)
+    insertar(dao,fechaActualizacion,valor)
 
 if __name__ == "__main__":
   main()
