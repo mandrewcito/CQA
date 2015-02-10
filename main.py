970f3b4dbcb58a8e6b2e6f4bbdff4fe854c0c954
mandrewcito <anbaalo@gmail.com>
2015-02-10 18:25:53 +0100

    muestra grafico ya

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class App():
     builder.connect_signals(self)
     self.window = builder.get_object("inicial")
     self.combobox_principal=builder.get_object("combobox_principal")
+    self.frameCentral=builder.get_object("frame_contenedor")
     #grafica 
     '''
     self.figure = Figure(figsize=(5,5), dpi=100)
@@ -44,7 +45,32 @@ class App():
 
   def on_combobox_principal_changed(self,w):
     # escoge el modelo (filas), selcciona la activa, y elige la columna de la que queremos el texto
-    print self.combobox_principal.get_model()[self.combobox_principal.get_active()][0]
+    opcion=self.combobox_principal.get_model()[self.combobox_principal.get_active()][0]
+    if (opcion=="Compra"):
+      GObject.idle_add(self.cargarEnContenedor,self.window)
+
+  def cargarEnContenedor(self,w):
+    fig = Figure(figsize=(5,5), dpi=100)
+    ax = fig.add_subplot(111, projection='polar')
+    #grafico ejemplo
+    N = 20
+    theta = linspace(0.0, 2 * pi, N, endpoint=False)
+    radii = 10 * random.rand(N)
+    width = pi / 4 * random.rand(N)
+
+    bars = ax.bar(theta, radii, width=width, bottom=0.0)
+
+    for r, bar in zip(radii, bars):
+      bar.set_facecolor(cm.jet(r / 10.))
+      bar.set_alpha(0.5)
+
+    ax.plot()
+    #grafico ejemplo
+    canvas = FigureCanvas(fig)
+    canvas.set_size_request(200,200)
+    self.frameCentral.pack_start(canvas, True, True, 0)
+    print "mostramos el grafico nuevo"
+    w.show_all()
 
 GObject.threads_init()
 App()
