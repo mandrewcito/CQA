ee508bef3a77c586c4a3291adbde3e997e76c05d
mandrewcito <anbaalo@gmail.com>
2015-02-22 21:13:54 +0100

    add test

---
+++ model/test/modelTest.py
@@ -0,0 +1,12 @@
+import unittest
+
+class modelTest(unittest.TestCase):
+
+  def testInsert(self):
+    #hacemos la insercion y vemos que nos devuelve la Id
+    self.assertEqual((1 + 2), 3)
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
+
